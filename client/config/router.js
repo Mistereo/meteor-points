5ca06e2ced5309fde5b84aa20cd10bee7b36fdfb
Mistereo <mistbox@ya.ru>
2015-01-24 21:10:55 +0500

    add title

--- client/config/router.js
+++ client/config/router.js
@@ -1,7 +1,11 @@
 Router.configure({
   layoutTemplate: 'base',
   loadingTemplate: 'loading',
-  notFoundTemplate: 'notFound'
+  notFoundTemplate: 'notFound',
+  onBeforeAction: function() {
+    document.title = "Игра точки!";
+    this.next();
+  }
 });
 
 Router.plugin('dataNotFound');
